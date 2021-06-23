35cab391ad0cd98b8ce831557d24cd11e4029dac
manish kumar <aimaanish1503@gmail.com>
2021-06-23 08:45:35 +0530

    redux thunk and action type implemented

--- src/components/NewCakeContainer.js
+++ src/components/NewCakeContainer.js
@@ -1,4 +1,4 @@
-import Reactm, {useState} from 'react'
+import React, {useState} from 'react'
 import { connect } from 'react-redux';
 import { buyCake } from '../redux';
 
--- src/redux/imp.js
+++ src/redux/imp.js
@@ -1,1 +1,2 @@
 //just install npm i redux-logger --save when you want to use redux-logger
+//npm i axios redux-thunk --save
